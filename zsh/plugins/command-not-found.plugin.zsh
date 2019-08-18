7b9caf2bb1d7b20d54bcbd9cfe77c5e65771b5f0
mmazzz <mmazzz123@gmail.com>
2019-08-18 14:09:25 +0900

    remove zsh

--- zsh/plugins/command-not-found.plugin.zsh
+++
@@ -1,9 +0,0 @@
-# CommandNotFound
-if [[ -x /usr/lib/command-not-found ]] ; then
-  if (( ! ${+functions[command_not_found_handler]} )) ; then
-    function command_not_found_handler {
-      [[ -x /usr/lib/command-not-found ]] || return 1
-      /usr/lib/command-not-found -- ${1+"$1"}
-    }
-  fi
-fi
