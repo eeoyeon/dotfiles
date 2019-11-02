8cee8ff4906a0dd8f8bd18f9a51dfa3d0011df28
mmazzz <mmazzz123@gmail.com>
2019-11-03 03:41:50 +0900

    pure

--- zsh/antigen.zsh
+++ zsh/antigen.zsh
@@ -3,11 +3,9 @@ source $DOTFILES/antigen/antigen.zsh
 # ADOTDIR=$HOME/.antigen
 
 antigen bundles <<EOBUNDLES
-  $ZSH/plugins --no-local-clone
   zsh-users/zsh-completions
   zsh-users/zsh-autosuggestions
   zsh-users/zsh-syntax-highlighting
-
   ## theme
   mafredri/zsh-async
   sindresorhus/pure
--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,11 +1,13 @@
 export PATH=$HOME/.local/bin:$PATH
 export LS_COLORS="fi=00:di=34:ow=34;40:ln=35:pi=30;44:so=35;44:do=35;44:bd=33;44:cd=37;44:or=05;37;41:mi=05;37;41:ex=01;31"
 
-DOTFILES=$HOME/.dotfiles
-ZSH=$DOTFILES/zsh
+export DOTFILES=$HOME/.dotfiles
+export ZSH=$DOTFILES/zsh
 
+fpath=($ZSH/completions $fpath)
 source $ZSH/config.zsh
 
+[[ -e /etc/zsh_command_not_found ]] && source /etc/zsh_command_not_found
 
 # autoload -U compinit && compinit
 
