749d196ace22a3d8f888f9c5159a113ac0f44877
mmazzz <mmazzz123@gmail.com>
2021-02-01 04:59:45 +0900

    fix plugin loading

--- fish/conf.d/config.fish
+++ fish/conf.d/config.fish
@@ -1,4 +1,11 @@
 set -x EDITOR vim
 
+set fisher_path $__fish_config_dir/plugins
+set fish_function_path $fisher_path/functions $fish_function_path
+set fish_complete_path $fisher_path/completions $fish_complete_path
+for source in $fisher_path/conf.d/*.fish
+  source $source
+end
+
 alias ll='ls -lh'
 alias la='ls -Alh'
--- install.fish
+++ install.fish
@@ -12,4 +12,8 @@ end
 
 ln -sfnv $DOTFILES/fish ~/.config/fish
 
+set fisher_path $__fish_config_dir/plugins
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
 fisher update
+
+echo "reload fish"
